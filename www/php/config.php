5bc21494d7b65482350c411230f1b63a0f63dc6e
Erhan YILDIRIM <erhan_yil@windowslive.com>
2017-07-31 17:59:56 +0300

    Update config.php

--- www/php/config.php
+++ www/php/config.php
@@ -5,11 +5,6 @@
 // $username = "root";
 // $password = "";
 
-$servername = "94.73.151.138";
-$database = "stdiosoft";
-$username = "stdiosoft";
-$password = "XYkm50O5";
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
 
